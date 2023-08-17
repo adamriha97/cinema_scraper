5f3603189d76a206b2558e60e7df75e96b506d58
Adam Říha <adam.riha97@gmail.com>
2023-08-17 17:32:05 +0200

    test selenium aero .py created

---
+++ test_selenium_aero.py
@@ -0,0 +1,26 @@
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1920,1080")
+
+#options = ChromeOptions()
+#options.headless = True
+driver = Chrome(options=options)
+driver.get('https://www.kinoaero.cz/?sort=sort-by-data&cinema=2%2C3%2C7&hall=10%2C23%2C1%2C2%2C3') # https://www.kinoaero.cz/?sort=sort-by-data
+#driver.maximize_window()
+#driver.set_window_size(1920, 1080)
+time.sleep(5)
+driver.find_element(By.CLASS_NAME, 'custom-select__input-chevron').click()
+time.sleep(1)
+driver.find_element(By.CLASS_NAME, 'form-check-input').click()
+driver.find_element(By.CLASS_NAME, 'custom-select__input-chevron-active').click()
+time.sleep(5)
+#driver.find_element(By.CLASS_NAME, 'program__movie-name').click()
+projections = driver.find_element(By.ID, 'projections-ids').get_attribute('data-projections').split(',')
+for projection in projections[:-1]:
+    print(f'https://www.kinoaero.cz/?sort=sort-by-data&cinema=1%2C2%2C3%2C7&hall=10%2C23%2C1%2C2%2C3&projection={projection}')
+driver.quit()
